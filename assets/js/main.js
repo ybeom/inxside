9ffec3fe40bed775c5b8117927cc63549f91c11f
ybeom <ybeom.me@gmail.com>
2022-10-02 00:10:31 +0900

    button effect update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,53 @@
-if (window.innerWidth < 1280) {
-    let position = 0;
-    let maxPos, imageWidth, start_x, end_x;
-    if (window.innerWidth >= 768) {
-        maxPos = 2;
-        imageWidth = 600;
-    } else {
-        maxPos = 3;
-        imageWidth = 320;
-    }
-
-    let curPos = maxPos;
-    const loadmapImage = document.querySelector(".loadmap-image");
-    loadmapImage.addEventListener("touchstart", touch_start);
-    loadmapImage.addEventListener("touchend", touch_end);
-
-    function prev() {
-        if (curPos > 1) {
-            if (position > 1200) {
-                position = 1200;
-            } else {
-                position += imageWidth;
-            }
-            loadmapImage.style.transform = `translateX(${position}px)`;
-            curPos -= 1;
+const loadmapImage = document.querySelector(".loadmap-image");
+if (loadmapImage) {
+    if (window.innerWidth < 1280) {
+        let position = 0;
+        let maxPos, imageWidth, start_x, end_x;
+        if (window.innerWidth >= 768) {
+            maxPos = 2;
+            imageWidth = 600;
+        } else {
+            maxPos = 3;
+            imageWidth = 320;
         }
-    }
-    function next() {
-        if (curPos < maxPos) {
-            if (position < 0) {
-                position = 0;
-            } else {
-                position -= imageWidth;
-            }
-            loadmapImage.style.transform = `translateX(${position}px)`;
-            curPos += 1;
-        }
-    }
-    function touch_start(e) {
-        start_x = e.touches[0].pageX;
-    }
 
-    function touch_end(e) {
-        end_x = e.changedTouches[0].pageX;
-        if (start_x > end_x) {
-            next();
-        } else if (start_x < end_x) {
-            prev();
+        let curPos = maxPos;
+        loadmapImage.addEventListener("touchstart", touch_start);
+        loadmapImage.addEventListener("touchend", touch_end);
+
+        function prev() {
+            if (curPos > 1) {
+                if (position > 1200) {
+                    position = 1200;
+                } else {
+                    position += imageWidth;
+                }
+                loadmapImage.style.transform = `translateX(${position}px)`;
+                curPos -= 1;
+            }
+        }
+        function next() {
+            if (curPos < maxPos) {
+                if (position < 0) {
+                    position = 0;
+                } else {
+                    position -= imageWidth;
+                }
+                loadmapImage.style.transform = `translateX(${position}px)`;
+                curPos += 1;
+            }
+        }
+        function touch_start(e) {
+            start_x = e.touches[0].pageX;
+        }
+
+        function touch_end(e) {
+            end_x = e.changedTouches[0].pageX;
+            if (start_x > end_x) {
+                next();
+            } else if (start_x < end_x) {
+                prev();
+            }
         }
     }
 }
@@ -147,6 +149,7 @@ if (feelingFilterEl) {
                 });
                 let isAllHidden = true;
                 let isAllUnclicked = true;
+
                 [].forEach.call(artCommentEls, (el) => {
                     isAllHidden = isAllHidden && el.classList.contains("hidden");
                 });
