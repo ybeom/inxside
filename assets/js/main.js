75de4e279587a29e72142e38b1a48e29d3f48bfa
ybeom <ybeom.me@gmail.com>
2022-10-04 21:55:19 +0900

    loadmap scroll setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,75 @@
-const loadmapImage = document.querySelector(".loadmap-image");
-if (loadmapImage) {
-    if (window.innerWidth < 1280) {
-        let position = 0;
-        let maxPos, imageWidth, start_x, end_x;
-        if (window.innerWidth >= 768) {
-            maxPos = 2;
-            imageWidth = 600;
-        } else {
-            maxPos = 3;
-            imageWidth = 320;
-        }
+const loadmapEl = document.querySelector(".loadmap");
+const loadmapImageEl = document.querySelector(".loadmap-image");
 
-        let curPos = maxPos;
-        loadmapImage.addEventListener("touchstart", touch_start);
-        loadmapImage.addEventListener("touchend", touch_end);
+const htmlEl = document.querySelector("html");
+const containerEl = document.querySelector(".container");
+console.dir(loadmapEl);
+console.log(window.innerWidth);
 
-        function prev() {
-            if (curPos > 1) {
-                if (position > 1200) {
-                    position = 1200;
-                } else {
-                    position += imageWidth;
-                }
-                loadmapImage.style.transform = `translateX(${position}px)`;
-                curPos -= 1;
-            }
-        }
-        function next() {
-            if (curPos < maxPos) {
-                if (position < 0) {
-                    position = 0;
-                } else {
-                    position -= imageWidth;
-                }
-                loadmapImage.style.transform = `translateX(${position}px)`;
-                curPos += 1;
-            }
-        }
-        function touch_start(e) {
-            start_x = e.touches[0].pageX;
-        }
+if (loadmapEl) {
+    if (window.innerWidth < 1280 && window.innerWidth > 700) {
+        console.log(1);
 
-        function touch_end(e) {
-            end_x = e.changedTouches[0].pageX;
-            if (start_x > end_x) {
-                next();
-            } else if (start_x < end_x) {
-                prev();
-            }
-        }
+        loadmapEl.scrollLeft = 600;
+    } else if (window.innerWidth < 700) {
+        console.log(2);
+        loadmapEl.scrollLeft = 700;
     }
 }
+
+// 슬라이딩 방식 아카이브
+// if (loadmapImage) {
+//     if (window.innerWidth < 1280) {
+//         let position = 0;
+//         let maxPos, imageWidth, start_x, end_x;
+//         if (window.innerWidth >= 768) {
+//             maxPos = 2;
+//             imageWidth = 600;
+//         } else {
+//             maxPos = 3;
+//             imageWidth = 320;
+//         }
+
+//         let curPos = maxPos;
+//         loadmapImage.addEventListener("touchstart", touch_start);
+//         loadmapImage.addEventListener("touchend", touch_end);
+
+//         function prev() {
+//             if (curPos > 1) {
+//                 if (position > 1200) {
+//                     position = 1200;
+//                 } else {
+//                     position += imageWidth;
+//                 }
+//                 loadmapImage.style.transform = `translateX(${position}px)`;
+//                 curPos -= 1;
+//             }
+//         }
+//         function next() {
+//             if (curPos < maxPos) {
+//                 if (position < 0) {
+//                     position = 0;
+//                 } else {
+//                     position -= imageWidth;
+//                 }
+//                 loadmapImage.style.transform = `translateX(${position}px)`;
+//                 curPos += 1;
+//             }
+//         }
+//         function touch_start(e) {
+//             start_x = e.touches[0].pageX;
+//         }
+
+//         function touch_end(e) {
+//             end_x = e.changedTouches[0].pageX;
+//             if (start_x > end_x) {
+//                 next();
+//             } else if (start_x < end_x) {
+//                 prev();
+//             }
+//         }
+//     }
+// }
 
 //코멘트 폼 관련
 const feelingBtnEl = document.querySelector(".comment-feeling-buttons-wrap");
@@ -71,10 +90,8 @@ if (feelingBtnEl) {
 }
 
 //갤러리 가로 스크롤
-const htmlEl = document.querySelector("html");
 const galleryContainerEl = document.querySelector(".gallery-container");
 const galleryWrapEl = document.querySelector(".gallery-wrap");
-const containerEl = document.querySelector(".container");
 
 if (galleryContainerEl) {
     const sliderWidth = galleryWrapEl.offsetWidth;
@@ -89,9 +106,6 @@ if (galleryContainerEl) {
             galleryContainerEl.scrollLeft += e.deltaY;
 
             containerEl.classList.add("container-fixed");
-
-            console.log("left: ", galleryContainerEl.scrollLeft);
-            console.log("top: ", htmlEl.scrollTop);
         } else {
             containerEl.classList.remove("container-fixed");
         }
